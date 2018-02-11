0a90fe6e95f33d258755784e6d115dac2de8349c
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2018-02-12 00:19:10 +0100

    Add ability to draw spanning word definition in sheet

--- backend/src/combine_and_shorten_definition.py
+++ backend/src/combine_and_shorten_definition.py
@@ -1,6 +1,12 @@
 from reportlab.pdfbase.pdfmetrics import stringWidth
 from exceptions import GenException
 
+class Result:
+    # num_words: number of words used in text
+    def __init__(self, text, num_words):
+        self.text = text;
+        self.num_words = num_words;
+
 # sep: string separating translations in definition
 # definition: list of translations (strings)
 def combine_and_shorten_definition(definition, sep, max_w, font_name, font_size):
@@ -9,5 +15,5 @@ def combine_and_shorten_definition(definition, sep, max_w, font_name, font_size)
     text = sep.join(definition);
     w = stringWidth(text, font_name, font_size);
     if w <= max_w:
-        return text;
+        return Result(text, len(definition));
     return combine_and_shorten_definition(definition[0:-1], sep, max_w, font_name, font_size);
--- backend/src/draw_word_definition.py
+++ backend/src/draw_word_definition.py
@@ -2,61 +2,71 @@ from reportlab.pdfgen import canvas
 from reportlab.pdfbase.pdfmetrics import stringWidth
 from combine_and_shorten_definition import combine_and_shorten_definition
 
+# TODO: rename file to sth like draw.py
+
 #TODO: remove
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
 from reportlab.lib.pagesizes import letter, A4
 
-# definition: list of translations (strings)
-def draw_word_definition(canvas, font_name, font_size, xfrom, yfrom, xto, yto, definition):
-    # assuming xfrom<xto, yfrom<yto
-
-    # draw summation symbol
-
-    offset = 2;
-    text_height = 7;
-    xstart = xfrom+text_height+offset;
-
-    curve = get_closed_summation_curve(xstart, yfrom, xto, yto);
-    draw_summation_curve(canvas, curve);
-
-    # draw definition
+# centers the text to ymid
+def draw_vertical_text(canvas, font_name, font_size, xto, ymid, text):
+    tw = stringWidth(text, font_name, font_size);
     canvas.setFont(font_name, font_size);
     canvas.saveState();
-    canvas.translate((xfrom+text_height), yfrom);
+    canvas.translate(xto, ymid - tw/2);
     canvas.rotate(90);
-    w = yto - yfrom;
-    text = combine_and_shorten_definition(definition, ', ', w, font_name, font_size)
-    tw = stringWidth(text, font_name, font_size);
-    canvas.drawString(w/2 - tw/2, 0, text);
+    canvas.drawString(0, 0, text);
     canvas.restoreState();
 
-def draw_summation_curve(canvas, curve):
+def draw_full_summation_curve(canvas, xfrom, yfrom, xto, yto):
+    # assuming xfrom<xto and yfrom<yto
+    curve = __get_closed_summation_curve(xfrom, yfrom, xto, yto);
+    __draw_summation_curve(canvas, curve);
+
+def draw_top_summation_curve(canvas, xfrom, yfrom, xto, yto):
+    # assuming xfrom<xto and yfrom<yto
+    curve = __get_opened_summation_curve(xfrom, yto, xto, yfrom);
+    __draw_summation_curve(canvas, curve);
+
+def draw_bottom_summation_curve(canvas, xfrom, yfrom, xto, yto):
+    # assuming xfrom<xto and yfrom<yto
+    curve = __get_opened_summation_curve(xfrom, yfrom, xto, yto);
+    __draw_summation_curve(canvas, curve);
+
+def draw_opened_top_summation_curve(canvas, xfrom, yfrom, xto, yto):
+    # assuming xfrom<xto and yfrom<yto
+    w = xto-xfrom;
+    curve = [__get_opened_curve(xto, yto, xfrom+w/2, yfrom)];
+    __draw_summation_curve(canvas, curve);
+
+def __draw_summation_curve(canvas, curve):
     for subcurve in curve:
         canvas.bezier(*subcurve);
 
-def get_closed_summation_curve(xfrom, yfrom, xto, yto):
+def __get_closed_summation_curve(xfrom, yfrom, xto, yto):
     curve = list();
     ymid = (yfrom+yto)/2;
-    curve.append(get_closed_curve(xfrom, ymid, xto, yto));
-    curve.append(get_closed_curve(xfrom, ymid, xto, yfrom));
+    curve.append(__get_closed_curve(xfrom, ymid, xto, yto));
+    curve.append(__get_closed_curve(xfrom, ymid, xto, yfrom));
     return curve;
 
-def get_opened_summation_curve(xfrom, yfrom, xto, yto):
+def __get_opened_summation_curve(xfrom, yfrom, xto, yto):
     curve = list();
     xmid = (xfrom+xto)/2;
     ymid = (yfrom+yto)/2;
-    curve.append(get_closed_curve(xfrom, ymid, xto, yfrom));
-    curve.append(get_opened_curve(xfrom, ymid, xmid, yto));
+    curve.append(__get_closed_curve(xfrom, ymid, xto, yfrom));
+    curve.append(__get_opened_curve(xfrom, ymid, xmid, yto));
     return curve;
 
-def get_closed_curve(xfrom, yfrom, xto, yto):
+def __get_closed_curve(xfrom, yfrom, xto, yto):
     return [xfrom,yfrom, xto,yfrom, xfrom,yto, xto,yto];
 
-def get_opened_curve(xfrom, yfrom, xto, yto):
+def __get_opened_curve(xfrom, yfrom, xto, yto):
     ymid = (yfrom+yto)/2;
     return [xfrom,yfrom, xto,yfrom, xto,ymid, xto,yto];
 
+# TODO: delete
 def summation_curve_demo(canvas):
     xfrom = 200;
     yfrom = 500;
