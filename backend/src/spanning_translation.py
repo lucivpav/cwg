d088477a05240438fb9c238566282024df591747
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2018-02-25 15:44:53 +0100

    Move SpanningTranslation to separate file

---
+++ backend/src/spanning_translation.py
@@ -0,0 +1,5 @@
+class SpanningTranslation:
+    # top and bottom translations: strings
+    def __init__(self, top_translation, bottom_translation):
+        self.top_translation = top_translation;
+        self.bottom_translation = bottom_translation;
