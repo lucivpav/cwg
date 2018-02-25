c213711faa1965e2f7a7287927b6588651ef2b7e
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2018-02-25 15:44:53 +0100

    Move SpanningTranslation to separate file

--- backend/src/gen.py
+++ backend/src/gen.py
@@ -110,13 +110,6 @@ class Guide(Enum):
     STAR = 2
     CROSS = 3
 
-# TODO: move to file!
-class SpanningTranslation:
-    # top and bottom translations: strings
-    def __init__(self, top_translation, bottom_translation):
-        self.top_translation = top_translation;
-        self.bottom_translation = bottom_translation;
-
 def get_character_json(file, character):
     while 1:
         line = file.readline();
