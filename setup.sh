66fc9eec7f3b1bb30b6afa2a8e535901a62a7988
Jonathan Beaulieu <123.jonathan@gmail.com>
2019-09-11 17:20:19 -0400

    Update setup.sh
    Co-Authored-By: Pavel Lučivňák <pavel.lucivnak@gmail.com>

--- setup.sh
+++ setup.sh
@@ -8,3 +8,4 @@ curl https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.txt.gz 
 mkdir frontend/tagmanager
 curl https://raw.githubusercontent.com/max-favilli/tagmanager/v3.0.2/tagmanager.js > frontend/tagmanager/tagmanager.js
 curl https://raw.githubusercontent.com/max-favilli/tagmanager/v3.0.2/tagmanager.css > frontend/tagmanager/tagmanager.css
+curl https://github.com/be5invis/source-han-sans-ttf/releases/download/v1.04.20170825/SourceHanSansTC-Normal.ttf > SourceHanSansTC-Normal.ttf
