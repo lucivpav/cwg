39466eae38389f4c667f86889b5937bb96342968
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2026-05-27 21:40:07 +0200

    improve setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,26 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-rm -rf cedict makemeahanzi frontend/tagmanager
+echo "Cleaning up previous setup..."
+rm -rf makemeahanzi cedict frontend/tagmanager SourceHanSansTC-Normal.ttf
 
-git clone https://github.com/skishore/makemeahanzi.git makemeahanzi
+echo "makemeahanzi..."
+curl https://github.com/skishore/makemeahanzi/archive/refs/heads/master.zip -L -o makemeahanzi.zip -sS
+unzip makemeahanzi.zip -q
+mv makemeahanzi-master makemeahanzi
+rm makemeahanzi.zip
+
+echo "cedict..."
 mkdir cedict
-curl https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.txt.gz | gunzip > cedict/data
-mkdir frontend/tagmanager
-curl https://raw.githubusercontent.com/max-favilli/tagmanager/v3.0.2/tagmanager.js > frontend/tagmanager/tagmanager.js
-curl https://raw.githubusercontent.com/max-favilli/tagmanager/v3.0.2/tagmanager.css > frontend/tagmanager/tagmanager.css
+curl https://www.mdbg.net/chinese/export/cedict/cedict_1_0_ts_utf-8_mdbg.txt.gz -sS | gunzip > cedict/data
 
-curl https://github.com/be5invis/source-han-sans-ttf/releases/download/v2.001.1/source-han-sans-ttf-2.001.1.7z -L > SourceHanSansTtf.7z
-7za e SourceHanSansTtf.7z SourceHanSansTC-Normal.ttf
+echo "tagmanager..."
+mkdir frontend/tagmanager
+curl https://raw.githubusercontent.com/max-favilli/tagmanager/v3.0.2/tagmanager.js -o frontend/tagmanager/tagmanager.js -sS
+curl https://raw.githubusercontent.com/max-favilli/tagmanager/v3.0.2/tagmanager.css -o frontend/tagmanager/tagmanager.css -sS
+
+echo "SourceHanSansTtf..."
+curl https://github.com/be5invis/source-han-sans-ttf/releases/download/v2.001.1/source-han-sans-ttf-2.001.1.7z -L -o SourceHanSansTtf.7z -sS
+7z e SourceHanSansTtf.7z SourceHanSansTC-Normal.ttf -bso0 -bsp0
 rm SourceHanSansTtf.7z
+
+echo "Done"
